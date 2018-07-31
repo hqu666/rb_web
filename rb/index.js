d8046a5d8ac3e70a2cb60efddb59d18c31ee11fb
hiroomi kuwayama <hkuwayama@coresoft-net.co.jp>
2018-08-01 00:04:52 +0900

    20

--- rb/index.js
+++ rb/index.js
@@ -1,10 +1,12 @@
+var dbMsg = "[../index.js]";
 
 const express = require('express');
 const app = express();
 const http = require('http').Server(app);
 const io = require('socket.io')(http);
 const port = process.env.PORT || 3080;
-
+dbMsg += ",http="+ http+",io="+ io+",port="+ port;
+//  "express=" + express +",app="+ app +
 
 app.use(express.static(__dirname + '/public'));
 
@@ -19,8 +21,8 @@ function onConnection(socket){
     socket.on('setautojudge', (data) => socket.broadcast.emit('setautojudge', data));
     socket.on('allclear', (data) => socket.broadcast.emit('allclear', data));		       		//全消去
 }
-
-io.on('connection', onConnection);
+//dbMsg += ",onConnection="+onConnection;
+io.on('connection', onConnection);          //onConnectionのソースを送る
 //urlの取得
 
 // http.listen(port, () => console.log('EC2>>> ec2-52-197-173-40.ap-northeast-1.compute.amazonaws.com:' + port));
@@ -28,4 +30,14 @@ io.on('connection', onConnection);
 var now = new Date();
 var UrlPparam =  "?sesion=" +  now.getYear() +(now.getMonth()+1)+now.getDate()+now.getHours()+now.getMinutes()+now.getSeconds();		// "?sesion="
 http.listen(port, () => console.log('web>>> '+  "http://127.0.0.1" +':' + port));       //cd H:\develop\xampp\htdocs\repireBrain\rb
-console.log('index.js : >>> '+  "http://127.0.0.1" +':' + port);
+console.log(dbMsg);
+// console.log('index.js : >>> '+  "http://127.0.0.1" +':' + port);
+
+
+/**
+個別セッション
+socket.ioで特定ユーザーにemitしたい時                http://nazomikan.hateblo.jp/entry/2014/06/05/031548
+Socket.ioを使ったチャットルーム ロジックの実装        https://qiita.com/ynunokawa/items/564757fe6dbe43d172f8
+Socket.ioでチャットルームを実現する方法               https://www.xmisao.com/2013/06/13/socketio-rooms.html
+socket.IOとHTML5 Canvasを用いた手書きチャットアプリを作ってみた      https://www.yoheim.net/blog.php?q=20120515
+*/
